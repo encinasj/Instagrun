126233692fff3645c9415ebf49a3576f09fced86
Jencinas <encinasj.angel@gmail.com>
2020-02-12 13:52:14 -0700

    in this commit added a update_profile.py file and django alloe me to configurate profiles

--- users/views.py
+++ users/views.py
@@ -2,13 +2,14 @@
 from django.contrib.auth import authenticate,login, logout
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-
 #exceptions
 from django.db.utils import IntegrityError
-
 #Models
 from django.contrib.auth.models import User
 from users.models import Profile
+#Form
+from users.forms import ProfileForm
+
 def login_views(request):
     #Login views
     if request.method == 'POST':
@@ -20,7 +21,6 @@ def login_views(request):
             return redirect('feed')
         else:
              return render(request,'users/login.html', {'error': 'Invaid username and password'})
-
     return render(request, 'users/login.html')
 
 def signup(request):
@@ -32,7 +32,6 @@ def signup(request):
 
         if passwd != passwd_confirmation:
             return render(request, 'users/signup.html', {'error': 'Password confirmatin doest not mach' })
-        
         try:
             user = User.objects.create_user(username=username, password=passwd)        
         except IntegrityError:
@@ -51,8 +50,33 @@ def signup(request):
 
 def update_profile(request):
     #Update a users profile views
-    return render(request,'users/update_profile.html')
-    
+    profile = request.user.profile
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            profile.website = data['website']
+            profile.phone_number = data['phone_number']
+            profile.biography = data['biography']
+            profile.picture = data['picture']
+            profile.save()
+
+            return redirect('update_profile')
+
+    else:
+        form = ProfileForm()
+
+    return render(
+        request=request,
+        template_name='users/update_profile.html',
+        context={
+            'profile': profile,
+            'user': request.user,
+            'form': form
+        }
+    )
 @login_required
 def logout_views(request):
     #loout_views
