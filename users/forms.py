686534937aefd7e53d0f221829abeff6c04a618c
Jencinas <encinasj.angel@gmail.com>
2020-02-18 15:24:07 -0700

    in this commit we validates a singup in forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,43 @@
 #Django
 from django import forms
+#models
+from django.contrib.auth.models import User
+from users.models import Profile
+
+class SignupForm(forms.Form):
+    #sginup Forms
+    username = forms.CharField(min_length=4, max_length=20,required=True)
+    password = forms.CharField(max_length=70, required=True, widget=forms.PasswordInput())
+    password_confirmation = forms.CharField(max_length=70, required=True, widget=forms.PasswordInput())
+
+    first_name = forms.CharField(min_length=2, max_length=50, required=True)
+    last_name = forms.CharField(min_length=2, max_length=50, required=True)
+    email = forms.EmailField(min_length=6, max_length=30, required=False, widget=forms.EmailInput())
+
+    def clean_username(self):
+        """Username must be unique."""
+        username = self.cleaned_data['username']
+        username_taken = User.objects.filter(username=username).exists()
+        if username_taken:
+            raise forms.ValidationError('Username is already in use.')
+        return username
+
+    def clean(self):
+        """Verify password confirmation match."""
+        data = super().clean()
+        password = data['password']
+        password_confirmation = data['password_confirmation']
+        if password != password_confirmation:
+            raise forms.ValidationError('Passwords do not match.')
+        return data
+
+    def save(self):
+        """Create user and profile."""
+        data = self.cleaned_data
+        data.pop('password_confirmation')
+        user = User.objects.create_user(**data)
+        profile = Profile(user=user)
+        profile.save()
 
 class ProfileForm(forms.Form):
     #Profile forms
